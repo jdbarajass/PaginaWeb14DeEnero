dd04ad0f325778ce4dd70f90745d1efeb0f124e9
jdbarajass <jdbarajass@gmail.com>
2022-01-18 19:06:13 -0500

    Agregar descripcion de experiencia

---
+++ pages/script.js
@@ -0,0 +1,31 @@
+// var clasificaciones = ["Ana", "Juan", "Raul", "Celia", "Maria", "Antonio"];
+// alert(
+//   clasificaciones[0] + "\n" + clasificaciones[1] + "\n" + clasificaciones[2]
+// );
+// console.log(
+//   clasificaciones[0],
+//   clasificaciones[1],
+//   clasificaciones[2],
+//   clasificaciones[3],
+//   clasificaciones[4],
+//   clasificaciones[5]
+// );
+// console.log(clasificaciones[0], clasificaciones[1], clasificaciones[2]);
+
+// clasificaciones = ["Marta","Ana","Roberto","Laura", "Juan", "Celia", "Raul", "Maria"];
+// console.log(
+//     clasificaciones[0],
+//     clasificaciones[1],
+//     clasificaciones[2],
+//     clasificaciones[3],
+//     clasificaciones[4],
+//     clasificaciones[5],
+//     clasificaciones[6],
+//     clasificaciones[7]
+//     );
+    
+// console.log(clasificaciones[0], clasificaciones[1], clasificaciones[2]);
+// alert(
+//   clasificaciones[0] + "\n" + clasificaciones[1] + "\n" + clasificaciones[2]
+// );
+    
